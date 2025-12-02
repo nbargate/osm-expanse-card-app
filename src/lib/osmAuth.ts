2ee113a4eb181828d5c02b6ee5c10d0e1c056046
nbargate <n.a.bargate@gmail.com>
2025-12-02 17:00:05 +0000

    Use Online Scout Manager OAuth defaults

--- src/lib/osmAuth.ts
+++ src/lib/osmAuth.ts
@@ -1,14 +1,20 @@
-const baseAuthorizationUrl = '';
+const DEFAULT_OSM_AUTH_BASE_URL = 'https://www.onlinescoutmanager.co.uk/oauth/authorize';
+
+const baseAuthorizationUrl = process.env.OSM_AUTH_BASE_URL ?? DEFAULT_OSM_AUTH_BASE_URL;
+const scopes = process.env.OSM_SCOPE ?? 'full';
 
 export function getAuthorizationUrl(): string {
   const params = new URLSearchParams({
     client_id: process.env.OSM_CLIENT_ID ?? '',
     redirect_uri: process.env.OSM_REDIRECT_URI ?? '',
     response_type: 'code',
-    scope: 'read_prefs write_api',
+    scope: scopes,
   });
 
-  return `${baseAuthorizationUrl}?${params.toString()}`;
+  const url = new URL(baseAuthorizationUrl);
+  url.search = params.toString();
+
+  return url.toString();
 }
 
 export async function exchangeCodeForTokens(code: string): Promise<any> {
